6eabead7da26b7a7f93afc0111134c2b4747907b
Christian Lussier <lussierc@allegheny.edu>
2021-02-28 16:52:07 -0500

    Allow user to choose between types of UI and add more welcome messages

--- src/SSP.py
+++ src/SSP.py
@@ -2,10 +2,18 @@
 
 import cml
 
+
 def main():
     """Runs the tool."""
 
-    cml.welcome_message() # prints the welcome message in the CML
+    cml.welcome_message()  # prints the welcome message in the CML
+
+    choice = cml.user_UI_choice()  # gets the user choice of UI to use
+    if choice == 1:
+        cml.run_cml()  # run the CML
+    elif choice == 2:
+        # run the web UI to be implemented later
+        print("This feature is not currently active.")
 
 
 main()
--- src/cml.py
+++ src/cml.py
@@ -1,5 +1,6 @@
 """Will house the Command Line Interface (CML) for the tool."""
 
+
 class color:
     """Defines different colors and text formatting settings to be used for CML output printing."""
 
@@ -14,29 +15,56 @@ class color:
     UNDERLINE = "\033[4m"
     END = "\033[0m"
 
+
 def welcome_message():
     """Prints the welcome message for the project."""
 
     print(
-    "\n\n   -----------------------------------------------"
-    + "\n   |                                             |\n"
-    + color.BOLD
-    + color.GREEN
-    + "   | Welcome to the StockSwingPredictor Program! |"
-    + color.END
-    + "\n   |                                             |"
-    + "\n   |  A tool that predicts stock price swings!   |"
-    + "\n   |                                             |"
-    + color.END
-    + "\n   -----------------------------------------------\n\n"
+        "\n\n   -----------------------------------------------"
+        + "\n   |                                             |\n"
+        + color.BOLD
+        + color.GREEN
+        + "   | Welcome to the StockSwingPredictor Program! |"
+        + color.END
+        + "\n   |                                             |"
+        + "\n   |  A tool that predicts stock price swings!   |"
+        + "\n   |                                             |"
+        + color.END
+        + "\n   -----------------------------------------------\n\n"
     )
+
+
+def user_UI_choice():
+    """Gets the user's choices to run the program with."""
+
+    user_choice = 0  # set default value for user UI choice
+
+    while not (user_choice == 1 or user_choice == 2):
+        user_choice = int(
+            input(
+                color.BOLD
+                + color.UNDERLINE
+                + "Would you like to run the program using the Command Line UI or the Web UI?"
+                + color.END
+                + "\n   - Enter 1 to use the CML. \n   - Enter 2 to use the Web App UI."
+                + color.GREEN
+                + "\n* Enter your choice: "
+                + color.END
+            )
+        )
+
+    return user_choice
 
 def cml_startup_message():
     """CML Interface startup message."""
     print(
-    "\n\n   -------------------------------------------\n"
-    + color.BOLD
-    + "   |      Starting the    CML version!       |"
-    + color.END
-    + "\n   -------------------------------------------\n\n"
+        "\n\n   -------------------------------------------\n"
+        + color.BOLD
+        + "   |      Starting the    CML version!       |"
+        + color.END
+        + "\n   -------------------------------------------\n\n"
     )
+
+
+def run_cml():
+    cml_startup_message()

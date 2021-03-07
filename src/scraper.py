229002bb278a695bc4ec81fb519fbf27160da126
Christian Lussier <lussierc@allegheny.edu>
2021-03-07 18:23:26 -0500

    Implement code to save content scraped by 'scraper.py' functions

--- src/scraper.py
+++ src/scraper.py
@@ -7,29 +7,50 @@ import yfinance as yf
 def scrape_stock_info(stocks):
     """Gathers real time stock prices and info from Yahoo! Finance."""
 
+    info_list = []
+
     for stock in stocks:
         ticker = yf.Ticker(stock)
 
+        info_list.append(ticker.info)
+
         print(ticker.info)
+
+    return info_list
 
 
 def scrape_stock_current_data(stocks):
     """Scrape 1day price info or last day's price info."""
+
+    current_data = []
 
     for stock in stocks:
         ticker = yf.Ticker(stock)
 
         # get historical market data
         current = ticker.history(period="1d")
+
+        current_data.append(current)
+
         print(current)
+
+    return current_data
+
 
 
 def scrape_historical_data(stocks):
     """Scrape historical stock price data from Yahoo! Finanace."""
+
+    historical_data = []
 
     for stock in stocks:
         ticker = yf.Ticker(stock)
 
         # get historical market data
         hist = ticker.history(period="1y")
-        print(hist)
+
+        historical_data.append(hist)
+
+        print(current)
+
+    return historical_data

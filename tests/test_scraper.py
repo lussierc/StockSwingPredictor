e8b8260f5d81d18a356283fe44667e8adcd290d3
Christian Lussier <lussierc@allegheny.edu>
2021-03-07 19:02:36 -0500

    Write basic test cases to ensure data is being scraped

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -1,12 +1,34 @@
 """Will test the functions of the scraper."""
 
-import scraper
+import pytest
+from src import scraper
 
-def test_scrape_stock_info():
+def test_basic_scrape_stock_info():
     """Tests to ensure that a dictionary of stock information is scraped."""
 
-def test_scrape_stock_current_data():
+    stocks = ['AAPL', 'DKNG', 'MSFT']
+
+    info_list = scraper.scrape_stock_info(stocks)
+
+    assert info_list is not None
+    assert len(info_list) == 3
+
+def test_basic_scrape_stock_current_data():
     """Tests to ensure that a dataframe of one-day stock price data is scraped."""
 
-def test_scrape_historical_data():
+    stocks = ['AAPL', 'DKNG', 'MSFT', 'DDOG']
+
+    data_list = scraper.scrape_stock_info(stocks)
+
+    assert data_list is not None
+    assert len(data_list) == 4
+
+def test_basic_scrape_historical_data():
     """Tests to ensure that a dataframe of historical stock price data is scraped."""
+
+    stocks = ['AAPL', 'DKNG', 'ZM']
+
+    data_list = scraper.scrape_stock_info(stocks)
+
+    assert data_list is not None
+    assert len(data_list) != 2

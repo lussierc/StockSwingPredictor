81d07162380700b30a12acf50296a9c121742aa7
Christian Lussier <lussierc@allegheny.edu>
2021-03-08 19:13:21 -0500

    Implement 'clean_historical_data()' to clean historical stock dates and prices for future prediction

--- src/data_cleaner.py
+++ src/data_cleaner.py
@@ -1,8 +1,26 @@
 """Contains data cleaning functions used throughout the project."""
 
+import numpy as np
+
+
 def scraped_df_cleaner(df):
     """Cleans data by fixing date column of scraped data."""
 
     df = df.reset_index()  # resets the df index so dates are a column
     print("TYPE", type(df))
     return df
+
+
+def clean_historical_data(historical_data):
+    """Prepares dates and prices for stock price prediction."""
+
+    dates = historical_data["Date"].to_numpy()
+    prices = historical_data["Close"].to_numpy()
+
+    dates = np.reshape(dates, (len(dates), 1))  # convert dates to a 1-d vector
+    prices = np.reshape(prices, (len(prices), 1))  # convert dates to a 1-d vector
+
+    print(dates)
+    print(prices)
+
+    return dates, prices
--- src/prediction.py
+++ src/prediction.py
@@ -1,17 +1,30 @@
 """Creates a model and performs a stock price swing prediction using ML methods."""
 import matplotlib.dates as mdates
+import data_cleaner
+from sklearn.svm import SVR
+import numpy as np
+
 
 def run_predictor(scraped_data):
     """Given scraped data, run the prediction models used by the tool."""
 
     print(scraped_data)
 
-    for stock in scraped_data:
-        print(stock['stock'])
-        historical_data = stock['stock_historical_data']
+    finalized_data = []
 
-        dates = historical_data['Date'].to_numpy()
-        prices = historical_data['Close'].to_numpy()
+    for stock_data in scraped_data:
+        print(stock_data["stock"])
 
-        print(dates)
-        print(prices)
+        stock_finalized_data = svr_prediction(stock_data)
+
+        finalized_data.append(stock_finalized_data)
+
+
+def svr_prediction(stock_data):
+    """Uses an SVR to predict stock prices."""
+
+    historical_data = stock_data["stock_historical_data"]
+
+    data_cleaner.clean_historical_data(historical_data)
+
+    return historical_data  # FIX THIS

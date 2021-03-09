a0e05719875f5a66d0114b0f3a0a8a48f6760b99
Christian Lussier <lussierc@allegheny.edu>
2021-03-08 19:03:05 -0500

    Prepare dates and prices for prediction

--- src/prediction.py
+++ src/prediction.py
@@ -1,6 +1,17 @@
 """Creates a model and performs a stock price swing prediction using ML methods."""
+import matplotlib.dates as mdates
 
 def run_predictor(scraped_data):
     """Given scraped data, run the prediction models used by the tool."""
 
     print(scraped_data)
+
+    for stock in scraped_data:
+        print(stock['stock'])
+        historical_data = stock['stock_historical_data']
+
+        dates = historical_data['Date'].to_numpy()
+        prices = historical_data['Close'].to_numpy()
+
+        print(dates)
+        print(prices)
--- src/scraper.py
+++ src/scraper.py
@@ -56,5 +56,5 @@ def data_cleaner(df):
     """Cleans data by fixing date column of scraped data."""
 
     df = df.reset_index()  # resets the df index so dates are a column
-
+    print("TYPE", type(df))
     return df

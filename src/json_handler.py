8be47ffc4a19e7fe88b031db723053148823f3fc
Christian Lussier <lussierc@allegheny.edu>
2021-03-28 18:10:14 -0400

    Reflect change in exporting/importing JSON files instead of CSVs

---
+++ src/json_handler.py
@@ -0,0 +1,11 @@
+"""Handles JSON files for the project."""
+
+
+def import_json():
+    """Imports a previously exported JSON file."""
+    # TODO
+
+
+def export_json():
+    """Exports a JSON file of tool prediction results."""
+    # TODO

b4595e387ecc1b94e47a9ff49645f9ce83e37920
Christian Lussier <lussierc@allegheny.edu>
2021-03-28 18:52:47 -0400

    Implement code to handle cases where imported files don't exist

--- src/json_handler.py
+++ src/json_handler.py
@@ -4,18 +4,21 @@ import json
 
 def append_json(prediction_results, import_file):
     """Imports a previously exported JSON file."""
-    file_data = json.load(open(import_file))
+    try:
+        file_data = json.load(open(import_file))
+        # convert data to list if not
+        if type(file_data) is dict:
+            file_data = [file_data]
 
-    # convert data to list if not
-    if type(file_data) is dict:
-        file_data = [file_data]
+        prediction_results.pop('figure', None) # remove the figure from the results to be exported
 
-    prediction_results.pop('figure', None) # remove the figure from the results to be exported
+        file_data.append(prediction_results)
 
-    file_data.append(prediction_results)
-
-    with open(import_file, 'w') as outfile:
-        json.dump(file_data, outfile)
+        with open(import_file, 'w') as outfile:
+            json.dump(file_data, outfile)
+    except:
+        # if no file is found, create and export a new one
+        export_json(prediction_results, import_file)
 
 def export_json(prediction_results, export_file):
     """Exports a JSON file of tool prediction results."""

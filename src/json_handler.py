95878374f2ed5546d13be5d4dd4c0aea964e54d1
Christian Lussier <lussierc@allegheny.edu>
2021-03-28 19:20:13 -0400

    Implement a function that specifically imports JSON files

--- src/json_handler.py
+++ src/json_handler.py
@@ -2,35 +2,35 @@
 
 import json
 
-
 def append_json(prediction_results, import_file):
     """Imports and appends new results to a previously exported JSON file."""
 
     try:
-        file_data = json.load(open(import_file))
+        file_data = import_json(import_file)
 
-        if type(file_data) is dict:  # convert imported data to list if not
+        if type(file_data) is dict: # convert imported data to list if not
             file_data = [file_data]
 
-        prediction_results.pop(
-            "figure", None
-        )  # remove the figure from the results to be exported
+        prediction_results.pop('figure', None) # remove the figure from the results to be exported
 
         file_data.append(prediction_results)
 
-        with open(import_file, "w") as outfile:
-            json.dump(file_data, outfile)
+        with open(import_file, 'w') as outfile:
+            json.dump(file_data, outfile)        
     except:
         # if no file is found, create and export a new one:
         export_json(prediction_results, import_file)
 
+def import_json(import_file):
+    """Imports a JSON file."""
+    imported_data = json.load(open(import_file))
+
+    return imported_data
 
 def export_json(prediction_results, export_file):
     """Exports to a new JSON file of generated tool prediction results."""
 
-    prediction_results.pop(
-        "figure", None
-    )  # remove the figure from the results to be exported
+    prediction_results.pop('figure', None) # remove the figure from the results to be exported
 
     with open(export_file, "w") as outfile:
         json.dump(prediction_results, outfile)

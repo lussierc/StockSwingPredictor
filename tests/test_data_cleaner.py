e159825e24f103717e79f6d5f1fb92b4221631e1
Christian Lussier <lussierc@allegheny.edu>
2021-04-03 00:46:41 -0400

    Write basic test case to ensure scraped data is properly cleaned

--- tests/test_data_cleaner.py
+++ tests/test_data_cleaner.py
@@ -1,4 +1,18 @@
 """Will test the functions of the data cleaner."""
 
 import pytest
-from src import data_cleaner
+import pandas as pd
+
+from src import data_cleaner, scraper
+
+def test_clean_scraped_prediction_data():
+    """Tests to ensure dates and prices scraped by the tool are prepped for ML predictions."""
+
+    data = scraper.scrape_stock_historical_data("AAPL", "1mo")
+
+    dates, prices = data_cleaner.clean_scraped_prediction_data(data)
+
+    assert prices is not None
+    assert dates[0] == 0
+    assert dates[1] == 1
+    assert prices is not None

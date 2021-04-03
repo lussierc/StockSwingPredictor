a4f784918405cd116c1026128595735c162cd12f
Christian Lussier <lussierc@allegheny.edu>
2021-04-02 23:51:21 -0400

    Fix test cases for scraper

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -42,7 +42,7 @@ def test_basic_scrape_historical_data():
 
     stock = "DDOG"
 
-    data = scraper.scrape_stock_historical_data(stock)
+    data = scraper.scrape_stock_historical_data(stock, "1mo")
 
     assert data is not None
 
@@ -52,7 +52,7 @@ def test_run_scraper():
 
     stocks = ["AAPL", "DKNG", "MSFT", "DDOG"]
 
-    scraped_data = scraper.perform_scraping(stocks)
+    scraped_data = scraper.perform_scraping(stocks, "1mo")
 
     stock_list_1 = scraped_data[0]
     stock_list_3 = scraped_data[2]

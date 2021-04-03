6193baee9f5400b5129eaf9e9b3eb629b421946c
Christian Lussier <lussierc@allegheny.edu>
2021-04-03 00:25:38 -0400

    Write test case to ensure data is properly appended to a JSON file

--- tests/test_json_handler.py
+++ tests/test_json_handler.py
@@ -3,6 +3,7 @@
 import pytest
 import os.path
 from os import path
+from shutil import copyfile
 
 from src import json_handler
 
@@ -31,3 +32,18 @@ def test_export_json():
 
 def test_append_to_json():
     """Tests to see if a JSON file can be imported correctly and then appended to."""
+
+    new_data = {"swing_predictions": {"svr_lin": "Down", "svr_poly": "Down", "svr_rbf": "Up", "lr": "Down", "en": "Down", "lasso": "Down", "knr": "Down"}, "next_day_predictions": {"svr_lin": 120.17706501172552, "svr_poly": 118.52845074304722, "svr_rbf": 123.97418079149483, "lr": 120.14958580658588, "en": 120.21123536097718, "lasso": 120.24794646232358, "knr": 121.53000030517578}, "prev_close": 123.0, "model_scores": {"svr_lin": 0.4523895926146947, "svr_poly": 0.5262019597177885, "svr_rbf": 0.8448432868159381, "lr": 0.4762592114122084, "en": 0.47623126623141654, "lasso": 0.4761880753418225, "knr": 0.9329122786743126}, "price_swing_prediction": "Down (1)", "svr_knr_price_avg": 122.7520905483353, "multi_fold_price_avg": 121.46625056605842, "data_time_period": "3mo", "date": "2021-04-03"}
+
+    import_file = "tests/testing_input_files/test_append2.json"
+
+    copyfile("tests/testing_input_files/test_append.json", import_file)
+
+    file_list = json_handler.append_json(new_data, import_file)
+
+    assert len(file_list) == 2
+    assert path.exists('tests/testing_input_files/test_append.json') == True
+
+    os.remove(import_file)
+
+    assert path.exists(import_file) == False 

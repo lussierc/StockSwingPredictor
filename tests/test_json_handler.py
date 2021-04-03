e0c241e0bffdd2ebfdccb57c12c4d33410d58d26
Christian Lussier <lussierc@allegheny.edu>
2021-04-03 00:03:35 -0400

    Write function to test if JSON files can be imported correctly

--- tests/test_json_handler.py
+++ tests/test_json_handler.py
@@ -2,3 +2,12 @@
 
 import pytest
 from src import json_handler
+
+def test_import_json():
+    """Tests to see if a JSON file can be imported correctly."""
+
+    import_file = "tests/testing_input_files/test_import.json"
+
+    imported_file_data = json_handler.import_json(import_file)
+
+    assert imported_file_data is not None
---
+++ tests/testing_input_files/README.md
@@ -0,0 +1,3 @@
+# Testing Input Files
+
+The testing input files are used by the Pytest test suite to ensure the JSON handler can properly import, export, and append to existing JSON files.

124e95fd45206f69677a9bbbf12ea64a532e3e9f
Christian Lussier <lussierc@allegheny.edu>
2021-04-04 12:57:45 -0400

    Test to ensure that models are properly scored

--- tests/test_prediction.py
+++ tests/test_prediction.py
@@ -70,8 +70,22 @@ def test_train_ml_models(dates, prices, svr_lin, svr_poly, svr_rbf, lr, en, lass
     assert knr is not None
 
 
+@pytest.mark.parametrize(
+    "dates, prices, svr_lin, svr_poly, svr_rbf, lr, en, lasso, knr",
+    [([[0], [1], [2], [3], [4], [5], [6], [7], [8], [9], [10], [11], [12], [13], [14], [15], [16], [17], [18], [19], [20], [21], [22]], [68.58000183105469, 65.75, 61.90999984741211, 59.52000045776367, 60.5, 62.20000076293945, 69.29000091552734, 71.61000061035156, 71.75, 67.75, 67.13999938964844, 70.6500015258789, 67.61000061035156, 71.9800033569336, 71.72000122070312, 69.75, 66.62999725341797, 65.86000061035156, 63.599998474121094, 58.20000076293945, 61.0, 61.33000183105469, 62.880001068115234], SVR(kernel="linear", C=1e3), SVR(kernel="poly", C=1e3, degree=2), SVR(kernel="rbf", C=1e3, degree=3, gamma="scale"), LinearRegression(), ElasticNet(), Lasso(), KNeighborsRegressor())],
+)
+def test_testscore_ml_models(dates, prices, svr_lin, svr_poly, svr_rbf, lr, en, lasso, knr):
+    """Tests to ensure that models are properly scored."""
 
-# def test_testscore_ml_models():
+    svr_lin, svr_poly, svr_rbf, lr, en, lasso, knr = prediction.train_ml_models(svr_lin, svr_poly, svr_rbf, lr, en, lasso, knr, dates, prices)
+
+    model_scores = prediction.test_ml_models(dates, prices, svr_lin, svr_poly, svr_rbf, lr, en, lasso, knr)
+
+    assert model_scores is not None
+
+    for key in model_scores.keys():
+        assert model_scores[key] is not 0.0 # ensure dictionary keys are still not set at the defailt value
+
 # def test_make_new_predictions():
 # def test_plot_predictions():
 # def test_predict_indiv_model_swing():

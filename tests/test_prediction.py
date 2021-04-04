908d7e13cd28346c2af87bdefd20ed46ca2f3087
Christian Lussier <lussierc@allegheny.edu>
2021-04-03 22:42:01 -0400

    Set base for potential prediction test functions in 'test_prediction()'

--- tests/test_prediction.py
+++ tests/test_prediction.py
@@ -2,3 +2,15 @@
 #
 # import pytest
 # from src import prediction
+
+def test_run_predictor():
+    """Tests to see if all predictions are properly completed."""
+
+# def test_ml_predictions():
+# def test_create_ml_models():
+# def test_train_ml_models():
+# def test_testscore_ml_models():
+# def test_make_new_predictions():
+# def test_plot_predictions():
+# def test_predict_indiv_model_swing():
+# def test_predict_price_swing():

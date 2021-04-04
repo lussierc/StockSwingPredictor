c01e721c0274d16a608b237f8cdc8b88057d998d
Christian Lussier <lussierc@allegheny.edu>
2021-04-04 19:38:26 -0400

    Test to ensure new predictions are properly made

--- tests/test_prediction.py
+++ tests/test_prediction.py
@@ -3,7 +3,7 @@
 import pytest
 import prediction
 
-
+import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.svm import SVR
 from sklearn.model_selection import train_test_split
@@ -91,13 +91,20 @@ def test_testscore_ml_models(dates, prices, svr_lin, svr_poly, svr_rbf, lr, en, 
     for key in model_scores.keys():
         assert model_scores[key] is not 0.0 # ensure dictionary keys are still not set at the defailt value
 
-# @pytest.mark.parametrize(
-#     "svr_lin, svr_poly, svr_rbf, lr, en, lasso, knr",
-#     [(SVR(kernel="linear", C=1e3), SVR(kernel="poly", C=1e3, degree=2), SVR(kernel="rbf", C=1e3, degree=3, gamma="scale"), LinearRegression(), ElasticNet(), Lasso(), KNeighborsRegressor(), [23])],
-# )
-# def test_make_new_predictions():
+@pytest.mark.parametrize(
+    "dates, prices, svr_lin, svr_poly, svr_rbf, lr, en, lasso, knr, next_date",
+    [([[0], [1], [2], [3], [4], [5], [6], [7], [8], [9], [10], [11], [12], [13], [14], [15], [16], [17], [18], [19], [20], [21], [22]], [68.58000183105469, 65.75, 61.90999984741211, 59.52000045776367, 60.5, 62.20000076293945, 69.29000091552734, 71.61000061035156, 71.75, 67.75, 67.13999938964844, 70.6500015258789, 67.61000061035156, 71.9800033569336, 71.72000122070312, 69.75, 66.62999725341797, 65.86000061035156, 63.599998474121094, 58.20000076293945, 61.0, 61.33000183105469, 62.880001068115234], SVR(kernel="linear", C=1e3), SVR(kernel="poly", C=1e3, degree=2), SVR(kernel="rbf", C=1e3, degree=3, gamma="scale"), LinearRegression(), ElasticNet(), Lasso(), KNeighborsRegressor(), [23])],
+)
+def test_make_new_predictions(dates, prices, svr_lin, svr_poly, svr_rbf, lr, en, lasso, knr, next_date):
+    svr_lin, svr_poly, svr_rbf, lr, en, lasso, knr = prediction.train_ml_models(svr_lin, svr_poly, svr_rbf, lr, en, lasso, knr, dates, prices)
 
-# def test_plot_predictions(): NOPE
+    next_date = np.reshape(next_date, (len(next_date), 1))
+
+    next_day_predictions = prediction.make_new_predictions(
+        svr_rbf, svr_lin, svr_poly, lr, en, lasso, knr, next_date
+    )
+
+    assert next_day_predictions is not None
 
 @pytest.mark.parametrize(
     "pred1, prev_close1, pred2, prev_close2, pred3, prev_close3",
